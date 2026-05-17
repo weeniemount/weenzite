faebc225706f49b839dfb545ae43bc56b763f94b
weenie <59868100+weeniemount@users.noreply.github.com>
2026-05-17 21:11:54 +0200

    horizon project wallpapers

--- build_files/build.sh
+++ build_files/build.sh
@@ -12,6 +12,11 @@ set -ouex pipefail
 # this installs a package from fedora repos
 dnf5 install -y tmux 
 
+# microwave gave me these commands
+dnf5 -y copr enable horizonproject/horizon
+dnf5 install -y horizon-backgrounds
+dnf5 -y copr disable horizonproject/horizon
+
 # Use a COPR Example:
 #
 # dnf5 -y copr enable ublue-os/staging
